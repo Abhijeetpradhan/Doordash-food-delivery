dcb474d56e88c6066e452269e71a267464684af0
abhijeet.pradhan <you@example.com>
2025-08-21 19:21:09 +0530

    Initial basic design

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <div>
+            <h2>Doordash </h2>
+            <h3>India’s #1 food delivery app</h3>
+            </div>
+            <p>Experience fast & easy online ordering on the Zomato app</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header

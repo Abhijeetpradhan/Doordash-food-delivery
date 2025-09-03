f823315c0873df50702a4d9772794190d8a468f4
Abhijeet Pradhan <abhijeetpradhan2001@gmail.com>
2025-09-03 13:40:13 +0530

    added counter functionality on food display

--- frontend/src/components/FoodItemcard/FoodItem.jsx
+++ frontend/src/components/FoodItemcard/FoodItem.jsx
@@ -5,12 +5,20 @@ import { assets } from "../../assets/assets";
 const FoodItem = ({ id, name, image, price, description }) => {
     console.log("name==>",name);
     console.log("image",image);
-    // const [foodItem,setFoodItem] = useState(0);  
+    const [itemCount,setItemCount] = useState(0);  
     
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="Image not found" />
+        {
+          !itemCount ? <img  className="add" src={assets.add_icon_white} alt="" onClick={()=>setItemCount(prev=>prev+1)} /> : 
+          <div className="food-item-counter">
+            <img src={assets.remove_icon_red} alt="" onClick={()=>setItemCount(prev=>prev-1)} />
+            <p>{itemCount}</p>
+            <img src={assets.add_icon_green} alt="" onClick={()=>setItemCount(prev=>prev+1)} />
+          </div>
+        }
       </div>
 
       <div className="food-item-info">
